8b7189a2f39324f07dcc74e9284c96316d6ffbda
Kresna <kresna@example.com>
2026-07-22 18:58:30 +0700

    Fix: smooth scroll & add new images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="id">
+    <html lang="id" data-scroll-behavior="smooth">
       <body className={inter.className}>
         <div className="flex flex-col min-h-screen">
           <Header />
